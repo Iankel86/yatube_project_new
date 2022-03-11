20dc3cf5cbaec8a0647af9a70c1a7fb9b109ff2e
Ян Шадрин <Yankelll86@mail.ru>
2022-03-11 20:16:36 +0300

    main

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,3 +1,21 @@
+from django.shortcuts import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+# Главная страница
+
+
+def index(request):
+#    return HttpResponse('Главная страница')
+    template = 'posts/index.html'
+    return render(request, template)
+
+# Страница с постами
+
+
+def group_posts(request, slug):
+    return HttpResponse(f'Страница с постом после фильтра по группе')
+
+def group_list(request):
+    template = 'posts/group_list.html'
+    return render(request, template)
