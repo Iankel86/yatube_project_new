55cf5db790d111417db075b69efc3e6572c20798
Ян Шадрин <Yankelll86@mail.ru>
2022-03-11 18:17:43 +0300

    main

---
+++ yatube/posts/urls.py
@@ -0,0 +1,13 @@
+# posts/urls.py
+from django.urls import path
+
+from . import views
+
+app_name = 'posts'
+
+urlpatterns = [
+    # Главная страница
+    path('', views.index),
+    path('group_list/', views.group_list),
+    path('group/<slug:slug>/', views.group_posts),
+]
